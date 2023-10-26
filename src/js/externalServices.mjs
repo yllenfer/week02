127f204413ec3c2e99a3ff3a7aff9d860b39cdc5
Morgan Mann <momomann4@gmail.com>
2023-10-26 14:10:36 -0600

    team activity 6 complete

---
+++ src/js/externalServices.mjs
@@ -0,0 +1,29 @@
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+export function getProductsByCategory(category = "tents") {
+  return fetch(`../json/${category}.json`)
+    .then(convertToJson)
+    .then((data) => data);
+}
+
+export async function findProductById(id) {
+  const products = await getProductsByCategory();
+  return products.find((item) => item.Id === id);
+}
+
+export async function checkout(payload) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+  return await fetch(baseURL + "checkout/", options).then(convertToJson);
+}
